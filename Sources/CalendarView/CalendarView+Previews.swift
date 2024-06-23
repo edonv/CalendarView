b52795e4d4b00159babeacf27e03f05878edd617
Edon <edon@valdman.works>
2024-06-23 13:59:42 -0500

    Update CalendarView+Previews.swift
    More changes for testing.

--- Sources/CalendarView/CalendarView+Previews.swift
+++ Sources/CalendarView/CalendarView+Previews.swift
@@ -10,33 +10,69 @@ import SwiftUI
 @available(iOS 17.0, *)
 private struct CalendarView_Preview: View {
     @State
-    private var selection = Set<DateComponents>()
+    private var selection = Array<DateComponents>()
+//    private var selection = Set<DateComponents>()
     
     @State
     private var buffer: DateComponents? = nil
+    
+    @State
+    private var toggle = false
+    
+    @State
+    private var visibleDateComponents: DateComponents? = nil
+//    private var visibleDateComponents: Binding<DateComponents> {
+//        .init {
+//            toggle ? .init() : .init(year: 1996, month: 4, day: 10)
+//        } set: { newValue/*, transaction*/ in
+//            
+//        }
+//    }
     
     var body: some View {
         VStack {
             Text("Test")
             
+            Toggle("Change Visible Date Components", isOn: $toggle)
+            
             Button("Add") {
-                guard let buffer,
-                      !selection.contains(buffer) else { return }
+//                guard let buffer,
+//                      !selection.contains(buffer) else { return }
+//                print(buffer)
                 withAnimation {
-                    selection.insert(buffer)
+//                    NotificationCenter.default.post(name: Notification.Name("test"), object: nil)
+                    
+//                    selection.insert(buffer)
+//                    selection.formUnion([
+                    var a = selection.first
+                    a?.day = 10
+                    var b = selection.first
+                    b?.day = 12
+                    var c = selection.first
+                    c?.day = 27
+                    selection.append(contentsOf: [
+                        a, b, c
+                    ].compactMap { $0 })
                 }
             }
             
-            CalendarView($selection)
+            CalendarView(
+                $selection
+//                visibleDateComponents: $visibleDateComponents,
+//                availableDateRange: .init(.now...)
+            )
                 .fontDesign(.default)
-                .decorations { dateComponents in
-                    return .image("star.fill", color: .orange)
-                }
+//                .decorations { dateComponents in
+//                    return .image("star.fill", color: .orange)
+//                }
                 .fixedSize()
         }
         .padding()
+//        .onChange(of: visibleDateComponents, initial: true) {
+//            print("visibleComponents:", visibleDateComponents)
+//        }
         .onChange(of: selection, initial: true) {
-            //            print(selection)
+            print("onChange:", selection.compactMap(\.day))
             if buffer == nil,
                let toBuffer = selection.first {
                 buffer = toBuffer
